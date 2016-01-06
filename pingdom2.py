d1002fd6be7030fc5b27222f6ba05d693c6c0368
Dawid Botha <dawid@fullfacing.com>
2016-01-06 17:28:08 +0200

    Smarter logging and logic

--- pingdom2.py
+++ pingdom2.py
@@ -9,7 +9,7 @@ import sendmail
 import sys
 
 timeout = 20
-mailFrom="example@production.server.com"
+mailFrom="example@example.com"
 recipients = ["example@example.com"]
 url=sys.argv[1]
 stringToCheckFor=sys.argv[2]
@@ -24,32 +24,31 @@ def getpage(url):
 lastDowntime = 'never'
 wasUpOnLastTest=True
 while True:
-    if stringToCheckFor not in getpage(url):
-        print ("page is down, double checking")
-        #The DoubleCheck™
+    if not wasUpOnLastTest:
         if stringToCheckFor not in getpage(url):
-            print ("okay site is definitely down!")
-            if wasUpOnLastTest:
-                print ("Just went down, emailing everyone")
+            print ("\t"+str(datetime.datetime.now())+": site is still down :(")
+        else:
+            #site seems to be back
+            print ("\t"+str(datetime.datetime.now())+": yay site is back up! Blasting the Masses with my email!")
+
+            sendmail.sendmail(mailFrom,
+                url + " is up",
+                "Your webiste, "+url+" is back up!\nThis is the first check that succeeded after "+str(lastDowntime),
+                recipients)
+            wasUpOnLastTest=True
+    else:
+        if stringToCheckFor not in getpage(url):
+            print (str(datetime.datetime.now())+": page is down, double checking")
+            #The DoubleCheck™
+            if stringToCheckFor not in getpage(url):
+                print ("\t"+str(datetime.datetime.now())+": okay site is definitely down!")
                 sendmail.sendmail(mailFrom,
                         url + " is down",
                         "Your webiste, "+url+" is down!\nThis is the first downtime since "+str(lastDowntime),
                         recipients)
                 wasUpOnLastTest = False
                 lastDowntime = datetime.datetime.now()
-            print ("site is still down :(")
+            else:
+                print ("\tOkay false alarm!")
 
-    else:
-        if not wasUpOnLastTest:
-            print ("yay site is back up! Blasting the Masses with my email!")
-
-            sendmail.sendmail(mailFrom,
-                    url + " is up",
-                    "Your webiste, "+url+" is back up!\nThis is the first check that succeeded after "+str(lastDowntime),
-                    recipients)
-
-            wasUpOnLastTest=True
-
-        else:
-            print ("all is still fine at "+str(datetime.datetime.now()))
     sleep(15)
